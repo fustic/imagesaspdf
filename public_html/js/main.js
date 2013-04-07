c2fe97ce2ea5b50b2eae2ef874519d5ce04702e0
Vadim Ivanov <ivanovvvadim@gmail.com>
2013-04-08 00:39:25 +0100

    images

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,17 +1,17 @@
 $(function(){
    $("#preview").imagesAsPdf({
        images:[
-           {source:"/ImagesAsPdf/img/page0001.jpg",title:"page 1"},
-           "/ImagesAsPdf/img/page0002.jpg",
-           "/ImagesAsPdf/img/page0003.jpg",
-           "/ImagesAsPdf/img/page0004.jpg",
-           "/ImagesAsPdf/img/page0005.jpg",
-           "/ImagesAsPdf/img/page0006.jpg",
-           "/ImagesAsPdf/img/page0007.jpg",
-           "/ImagesAsPdf/img/page0008.jpg"
+           {source:"/img/page0001.jpg",title:"page 1"},
+           "/img/page0002.jpg",
+           "/img/page0003.jpg",
+           "/img/page0004.jpg",
+           "/img/page0005.jpg",
+           "/img/page0006.jpg",
+           "/img/page0007.jpg",
+           "/img/page0008.jpg"
        ],
-       blankImage:"/ImagesAsPdf/img/grey.gif",
+       blankImage:"/img/grey.gif",
        scale:0.5,
-       downloadFileLink:"/ImagesAsPdf/img/page0008.jpg"
+       downloadFileLink:"/img/page0008.jpg"
    });
 });
